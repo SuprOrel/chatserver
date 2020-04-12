71c187f636c48645aa1415b5106cf3654f24f0da
SuprOrel <40279304+SuprOrel@users.noreply.github.com>
2020-04-12 17:08:16 +0300

    Single user message setup

--- src/main/java/com/springchat/chat/configuration/WebSocketConfiguration.java
+++ src/main/java/com/springchat/chat/configuration/WebSocketConfiguration.java
@@ -1,23 +1,49 @@
 package com.springchat.chat.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer {    @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/socket")
-                .setAllowedOrigins("*")
-                .withSockJS();
+    registry
+            .addEndpoint("/socket")
+            .setHandshakeHandler(new DefaultHandshakeHandler() {
+
+                public boolean beforeHandshake(
+                        ServerHttpRequest request,
+                        ServerHttpResponse response,
+                        WebSocketHandler wsHandler,
+                        Map attributes) throws Exception {
+
+                    if (request instanceof ServletServerHttpRequest) {
+                        ServletServerHttpRequest servletRequest
+                                = (ServletServerHttpRequest) request;
+                        HttpSession session = servletRequest
+                                .getServletRequest().getSession();
+                        attributes.put("sessionId", session.getId());
+                    }
+                    return true;
+                }})
+            .setAllowedOrigins("*")
+            .withSockJS();
     }
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app")
-                .enableSimpleBroker("/chat", "/login");
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic/", "/queue/");
+        config.setApplicationDestinationPrefixes("/app");
     }
 }
--- src/main/java/com/springchat/chat/controllers/WebSocketController.java
+++ src/main/java/com/springchat/chat/controllers/WebSocketController.java
@@ -1,14 +1,19 @@
 package com.springchat.chat.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
+import java.security.Principal;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class WebSocketController {
@@ -18,17 +23,22 @@ public class WebSocketController {
     WebSocketController(SimpMessagingTemplate template) {
         this.template = template;
     }
-    @MessageMapping("/send/message")
-    public void onReceivedMessage(String message){
-        this.template.convertAndSend("/chat", new SimpleDateFormat("HH:mm:ss").format(new Date())+ "- " + message);
-    }
-    @MessageMapping("/send/login")
-    public void onReceivedLogin(String message){
-        if(usernames.contains(message))
-            this.template.convertAndSend("/login", new SimpleDateFormat("HH:mm:ss").format(new Date())+ "- Occupied");
-        else {
+
+    @MessageMapping("/login")
+    @SendToUser("/queue/reply")
+    public String processMessageFromClient(
+            @Payload String message,
+            Principal principal) throws Exception {
+        if(usernames.contains(message)) return "Occupied";
+        else{
             usernames.add(message);
-            this.template.convertAndSend("/login", new SimpleDateFormat("HH:mm:ss").format(new Date())+ "- Logged in");
+            return message;
         }
+    }
+
+    @MessageExceptionHandler
+    @SendToUser("/queue/errors")
+    public String handleException(Throwable exception) {
+        return exception.getMessage();
     }
 }
