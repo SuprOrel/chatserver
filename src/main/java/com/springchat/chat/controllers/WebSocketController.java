bec8074d6d10027c2cf3fdcea53512d37ba9a3e4
SuprOrel <40279304+SuprOrel@users.noreply.github.com>
2020-04-27 10:09:09 +0300

    improved login and added register

--- src/main/java/com/springchat/chat/controllers/WebSocketController.java
+++ src/main/java/com/springchat/chat/controllers/WebSocketController.java
@@ -10,8 +10,10 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
 import java.security.Principal;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -30,33 +32,58 @@ public class WebSocketController {
     @MessageMapping("/login")
     @SendToUser("/queue/reply")
     public String processLoginFromClient(
-            @Payload String message,
-            Principal principal) throws Exception {
-        if(message.startsWith("delete ")) {
-            String username = message.substring(7);
-//            UserRepository.remove(username);
-            removeUser(username);
-            this.template.convertAndSend("/global", new SimpleDateFormat("HH:mm:ss").format(new Date())+ "- Server:deleted " + username);
-            return "deleted";
-        }
-        else {
-            String[] info = message.split(",");
-            if(info[0].equals("Server")) return "Login failed: Illegal username";
-            User user = getUser(info[0]);
-            if(user == null) {
-                if (info[0].length() > 10) return "Login failed: Username too long";
-                if (!Charset.forName("US-ASCII").newEncoder().canEncode(message))
-                    return "Login failed: English only";
-                if (!info[0].matches(".*[a-zA-Z]+.*")) return "Login failed: Username must contain letters";
-                if (!info[1].matches(".*[a-zA-Z]+.*")) return "Login failed: Password must contain letters";
-                addUser(info[0], info[1]);
-                this.template.convertAndSend("/global", new SimpleDateFormat("HH:mm:ss").format(new Date()) + "- Server:logged in " + info[0]);
-            }
-            else {
-                if(!user.getPassword().equals(info[1])) return "Login failed: Incorrect password";
-            }
-            return "logged in";
-        }
+            String message) throws Exception {
+        String[] split = message.split(",");
+        String mail = split[0], password = split[1];
+        User user = getUserFromMail(mail);
+        if(user == null) return "Login failed: User not found";
+        if(!user.getPassword().equals(password)) return "Login failed: Incorrect password";
+        return "Logged in: " + user.getName();
+//        if(message.startsWith("delete ")) {
+//            String username = message.substring(7);
+////            UserRepository.remove(username);
+//            removeUser(username);
+//            this.template.convertAndSend("/global", new SimpleDateFormat("HH:mm:ss").format(new Date())+ "- Server:deleted " + username);
+//            return "deleted";
+//        }
+//        else {
+//            String[] info = message.split(",");
+//            if(info[0].equals("Server")) return "Login failed: Illegal username";
+//            User user = getUser(info[0]);
+//            if(user == null) {
+//                if (info[0].length() > 10) return "Login failed: Username too long";
+//                if (!Charset.forName("US-ASCII").newEncoder().canEncode(message))
+//                    return "Login failed: English only";
+//                if (!info[0].matches(".*[a-zA-Z]+.*")) return "Login failed: Username must contain letters";
+//                if (!info[1].matches(".*[a-zA-Z]+.*")) return "Login failed: Password must contain letters";
+//                addUser(info[0], info[1]);
+//                this.template.convertAndSend("/global", new SimpleDateFormat("HH:mm:ss").format(new Date()) + "- Server:logged in " + info[0]);
+//            }
+//            else {
+//                if(!user.getPassword().equals(info[1])) return "Login failed: Incorrect password";
+//            }
+//            return "logged in";
+//        }
+    }
+
+    @MessageMapping("/register")
+    @SendToUser("/queue/reply")
+    public String processRegisterFromClient(
+            String message) throws Exception {
+        String[] split = message.split(",");
+        String mail = split[0], password = split[1], username = split[2];
+        if (username.length() > 10) return "Register failed: Username longer then 10 letters";
+        if (!username.matches(".*[a-zA-Z]+.*")) return "Register failed: Username must contain letters";
+        if (!password.matches(".*[a-zA-Z]+.*")) return "Register failed: Password must contain letters";
+        CharsetEncoder encoder = Charset.forName("US-ASCII").newEncoder();
+        if(!encoder.canEncode(username)) return "Register failed: Username must be in english";
+        if(!encoder.canEncode(password)) return "Register failed: Password must be in english";
+        if(isUsernameOccupied(username)) return "Register failed: Username occupied";
+        if(isMailOccupied(mail)) return "Register failed: Mail occupied";
+        addUser(username, password, mail);
+        this.template.convertAndSend("/global", new SimpleDateFormat("HH:mm:ss").format(new Date()) + "- Server:registerd " + username);
+        System.out.println("registerd " + username + " " + password + " " + mail);
+        return "Logged in: " + username;
     }
 
     boolean containsNonEnglish(String value) {
@@ -100,6 +127,12 @@ public class WebSocketController {
         }
         return null;
     }
+    public User getUserFromMail(String mail) {
+        for(User user : users.findAll()) {
+            if(user.getEmail().equals(mail)) return user;
+        }
+        return null;
+    }
 //    public int getUserId(String username) {
 //        for(User user : users.findAll()) {
 //            if(user.getName().equals(username)) return user.getId();
@@ -111,6 +144,10 @@ public class WebSocketController {
         return getUser(username) != null;
     }
 
+    public boolean isMailOccupied(String mail) {
+        return getUserFromMail(mail) != null;
+    }
+
     public void removeUser(String username) {
         removeUser(getUser(username));
     }
@@ -118,10 +155,11 @@ public class WebSocketController {
         users.delete(user);
     }
 
-    public void addUser(String username, String password) {
+    public void addUser(String username, String password, String mail) {
         User user = new User();
         user.setName(username);
         user.setPassword(password);
+        user.setEmail(mail);
         users.save(user);
     }
 }
